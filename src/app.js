bf8e96eb41d8b1228cc84e45a1174ce3a3e1cad2
Lại Huỳnh Khánh Duy <namhuyen42003@gmail.com>
2025-10-21 13:40:37 +0700

    CROS

--- src/app.js
+++ src/app.js
@@ -50,6 +50,20 @@ const GetServiceSuggestions = require('./application/bookings/suggestion');
 const app = express();
 
 // --- Middlewares ---
+// CORS middleware để cho phép frontend kết nối
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use(express.json());
 app.use(passport.initialize());
 
